fff29fd70c1abcc0bcda2a61b249e7a734ffe1c6
長島 雄平 <yu-nagashima@cyber-u.local>
2022-09-12 16:56:35 +0900

    index.jsを編集

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express 2022' });
+  res.render('index', { title: 'Express 2022-2' });
 });
 
 module.exports = router;
