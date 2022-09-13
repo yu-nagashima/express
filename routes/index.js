f41def807b940a897a1591a60bb5e65ea907530b
yu-nagashima <109783697+yu-nagashima@users.noreply.github.com>
2022-09-13 16:19:20 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express 2022-2' });
+  res.render('index', { title: 'Express 2022-3' });
 });
 
 module.exports = router;
